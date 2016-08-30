1ce2bd5c24bb093aa7102430b331e8e7c80505cf
Sofia Kwok <zazakwok@gmail.com>
2016-08-29 21:33:51 -0400

    adding comments and minor debugging

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -18,43 +18,48 @@ public:
 	enum Mode{ TANK_DRIVE, ARCADE_DRIVE };	
 
 private: 
-
+	//drive train
 	static const int LEFT_PWM_ONE = 0;
 	static const int LEFT_PWM_TWO = 4;
 	static const int RIGHT_PWM_ONE = 1;
 	static const int RIGHT_PWM_TWO = 3;
 	
+	//sensors n' things
 	static const int GYRO = 5;
 	static const int ENCODER_A = 6;
 	static const int ENCODER_B = 7;
 
-
+	//controllers
 	GamepadF310 * pilot;
 	GamepadF310 * copilot;
 
 	RobotDrive * drive;
 
+	//more drive train
 	VictorSP * frontLeft;
 	VictorSP * backLeft;
 	VictorSP * frontRight;
 	VictorSP * backRight;
 
+	//choosing between tank and arcade
 	SendableChooser * modeChooser;
 	Mode driveMode;
 
 	PowerDistributionPanel * pdp;
 	
+	//sensors!
 	BuiltInAccelerometer * acceler;
 	Lib830::AnalogGyro * gyro;
-
 	Encoder * encoder;
 
+	//cameras yay
 	CAMERAFEEDS * cameraFeeds;
 
 	static const int TICKS_TO_FULL_SPEED = 15;
 	
 	void RobotInit() {
 
+		//drive train!
 		drive = new RobotDrive(
 			new VictorSP(LEFT_PWM_ONE),
 			new VictorSP(LEFT_PWM_TWO),
@@ -62,23 +67,26 @@ private:
 			new VictorSP(RIGHT_PWM_TWO)
 		);
 
+		//controllers
 		pilot = new GamepadF310(0);
 		copilot = new GamepadF310(1);
 
 		pdp = new PowerDistributionPanel();
 
+		//putting the option to choose between the arcade and tank drives on the smart dashboard
 		modeChooser = new SendableChooser();
 		modeChooser-> AddDefault("Arcade Drive", new Mode(ARCADE_DRIVE));
 		modeChooser-> AddObject("Tank Drive", new Mode(TANK_DRIVE));
-
 		SmartDashboard::PutData("Moder Chooser", modeChooser);		
 		
+		//declaring all our sensors
 		gyro = new Lib830::AnalogGyro(GYRO);
 
 		acceler = new BuiltInAccelerometer;
 
 		encoder = new Encoder(ENCODER_A, ENCODER_B);
 
+		//declaring camera stuff
 		cameraFeeds = new CAMERAFEEDS;
 
 		cameraFeeds->init();
@@ -101,29 +109,32 @@ private:
 
 	void TeleopPeriodic()
 	{
+		//switching between the different drive modes (Tank, Arcade)
 		switch(driveMode) {
+			//tank drive
 			case TANK_DRIVE:
 				float leftforward = accel(leftforward, pilot->LeftY(), TICKS_TO_FULL_SPEED);
 				float rightforward = accel(leftforward, pilot->RightY(), TICKS_TO_FULL_SPEED);
 				drive->TankDrive(leftforward,rightforward,true);
-				break
+				break;
 
-			case ARCADE_DRIVE:
-
-			default: 
+			//arcade drive
+			default:
 				float targetForward = pilot ->LeftY();
 				float turn = pilot->RightX()/1.4;
-
 				float forward = accel(previous_forward, targetForward, TICKS_TO_FULL_SPEED);
 				drive->ArcadeDrive(forward, turn, true);
-				break
+				break;
 
+		//putting data on the smart dashboard
 		SmartDashboard::PutData("gyro", gyro);
 
 		SmartDashboard::PutNumber("accelerometer Z", acceler->GetZ());
 
 		SmartDashboard::PutNumber("Encoder", encoder->Get());
 
+		//camera feed stuff - switching cameras
+		//useless for now since we only have one camera on there...
 		if (pilot->ButtonState(GamepadF310::BUTTON_X)) {
 			cameraFeeds-> changeCam(cameraFeeds->kBtCamFront);
 		}
